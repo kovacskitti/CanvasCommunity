de7f69e8c9603855ebe06c8b532f5995ba36c1e5
kovacskitti <kovacskitti001@gmail.com>
2024-03-17 08:50:18 +0100

    docs: add Artist model

---
+++ CanvasCommunityBE/CanvasCommunity/Model/Artist.cs
@@ -0,0 +1,15 @@
+namespace CanvasCommunity;
+
+public class Artist
+{
+    public string Name { set; get; }
+    public List<Painting> Paintings { set; get; }
+    public string Description { set; get; }
+    public string Gender { set; get; }
+    public string Biography { set; get; }
+    public int Birthday { set; get; }
+    public int Deathday { set; get; }
+    public string Hometown { set; get; }
+    public string Location { set; get; }
+    public string Nationality { set; get; }
+  }
