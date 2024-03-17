70c90eca8812fae691866c6125017837630d94cd
kovacskitti <kovacskitti001@gmail.com>
2024-03-17 08:50:02 +0100

    docs: add Painting model

---
+++ CanvasCommunityBE/CanvasCommunity/Model/Painting.cs
@@ -0,0 +1,9 @@
+namespace CanvasCommunity;
+
+public class Painting
+{
+    public string Title { set; get; }
+    public string Artist { set; get; }
+    public int Year { set; get; }
+    public string Description { set; get; }
+}
