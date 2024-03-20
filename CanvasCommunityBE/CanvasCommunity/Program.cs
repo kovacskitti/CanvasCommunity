cf5f8eedeb64a0e4b84a4706894f86fe7a2ba9ab
kovacskitti <kovacskitti001@gmail.com>
2024-03-20 13:26:42 +0100

    refact: add options parameter to the AddDbContext

--- CanvasCommunityBE/CanvasCommunity/Program.cs
+++ CanvasCommunityBE/CanvasCommunity/Program.cs
@@ -1,10 +1,13 @@
+using CanvasCommunity.Context;
 using CanvasCommunity.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 AddServices();
 
 builder.Services.AddControllers();
+AddDbContext();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
@@ -27,4 +30,12 @@ void AddServices()
 {
     builder.Services.AddHttpClient();
     builder.Services.AddScoped<IArtsyTokenManager,ArtsyTokenManager>();
+}
+
+void AddDbContext()
+{
+    
+    builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 }
