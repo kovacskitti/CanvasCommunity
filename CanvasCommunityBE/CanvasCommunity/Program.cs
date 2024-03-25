1be364491c016154b3b0e29eb8e29cecbb8f1c68
kovacskitti <kovacskitti001@gmail.com>
2024-03-25 19:35:55 +0100

    feat: add new services( authentication, identity)

--- CanvasCommunityBE/CanvasCommunity/Program.cs
+++ CanvasCommunityBE/CanvasCommunity/Program.cs
@@ -1,11 +1,17 @@
+using System.Text;
 using CanvasCommunity.Context;
 using CanvasCommunity.Services;
+using CanvasCommunity.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 AddServices();
-
+AddAuthentication();
+AddIdentity();
 builder.Services.AddControllers();
 AddDbContext();
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -30,6 +36,7 @@ void AddServices()
 {
     builder.Services.AddHttpClient();
     builder.Services.AddScoped<IArtsyTokenManager,ArtsyTokenManager>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
 }
 
 void AddDbContext()
@@ -38,4 +45,44 @@ void AddDbContext()
     builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
+    builder.Services.AddDbContext<UsersContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JwtSettings:ValidIssuer"],
+                ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["JwtIssuerSigningKey"])),
+            };
+        });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<UsersContext>();
 }
