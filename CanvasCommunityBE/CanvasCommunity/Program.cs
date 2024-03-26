b42a35b4be5f56b89bdd976dff16393f18159bdb
kovacskitti <kovacskitti001@gmail.com>
2024-03-26 18:08:58 +0100

    feat: add new installation, registration for program.cs

--- CanvasCommunityBE/CanvasCommunity/Program.cs
+++ CanvasCommunityBE/CanvasCommunity/Program.cs
@@ -6,22 +6,23 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 AddServices();
+ConfigureSwagger();
+AddDbContext();
 AddAuthentication();
 AddIdentity();
-builder.Services.AddControllers();
-AddDbContext();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -35,8 +36,11 @@ app.Run();
 void AddServices()
 {
     builder.Services.AddHttpClient();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddScoped<IArtsyTokenManager,ArtsyTokenManager>();
     builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
 }
 
 void AddDbContext()
@@ -85,4 +89,35 @@ void AddIdentity()
             options.Password.RequireLowercase = false;
         })
         .AddEntityFrameworkStores<UsersContext>();
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please, enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[] { }
+            }
+        });
+    });
 }
