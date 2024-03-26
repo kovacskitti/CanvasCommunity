0fdeb83ad0b9b3aa3019ebd35778927aaa173117
kovacskitti <kovacskitti001@gmail.com>
2024-03-26 18:07:24 +0100

    feat: add login service method

--- CanvasCommunityBE/CanvasCommunity/Services/Authentication/AuthService.cs
+++ CanvasCommunityBE/CanvasCommunity/Services/Authentication/AuthService.cs
@@ -6,10 +6,13 @@ namespace CanvasCommunity.Services.Authentication;
 public class AuthService : IAuthService
 {
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly ITokenService _tokenService;
+    
 
-    public AuthService(UserManager<IdentityUser> userManager)
+    public AuthService(UserManager<IdentityUser> userManager, ITokenService tokenService)
     {
         _userManager = userManager;
+        _tokenService = tokenService;
     }
 
     public async Task<AuthResult> RegisterAsync(string email, string username, string password)
@@ -34,5 +37,32 @@ public class AuthService : IAuthService
 
         return authResult;
 
+    }
+    
+    public async Task<AuthResult> LoginAsync(string email, string password)
+    {
+        var managedUser = await _userManager.FindByEmailAsync(email);
+
+        if (managedUser == null)
+        {
+            return InvalidInput(email);
+        }
+
+        var isPasswordValid = await _userManager.CheckPasswordAsync(managedUser, password);
+        if (!isPasswordValid)
+        {
+            return InvalidInput(email);
+        }
+
+        var accessToken = _tokenService.CreateToken(managedUser);
+
+        return new AuthResult(true, managedUser.Email, managedUser.UserName, accessToken);
+    }
+
+    private static AuthResult InvalidInput(string email)
+    {
+        var result = new AuthResult(false, email, "", "");
+        result.ErrorMessages.Add("Bad credentials","Invalid data");
+        return result;
     }
 }
--- CanvasCommunityBE/CanvasCommunity/Services/Authentication/IAuthService.cs
+++ CanvasCommunityBE/CanvasCommunity/Services/Authentication/IAuthService.cs
@@ -3,4 +3,6 @@ namespace CanvasCommunity.Services.Authentication;
 public interface IAuthService
 {
     Task<AuthResult> RegisterAsync(string email, string username, string password);
+
+    Task<AuthResult> LoginAsync(string email, string password);
 }
